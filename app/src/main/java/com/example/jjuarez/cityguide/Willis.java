0e437872ecf4f3da39bf2949b4264d4b8ac10b14
Jose Juarez <jjuarez5631@students.mchenry.edu>
2018-03-04 21:57:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/jjuarez/cityguide/Willis.java
@@ -0,0 +1,15 @@
+package com.example.jjuarez.cityguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Willis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_willis);
+
+
+    }
+}
